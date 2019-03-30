b259ff6f6eef28ac7cd08c5dace722838acc5d6c
Food Factory Business Automation Home <ffba@labshome.co>
2019-03-30 08:27:08 +0100

    +google group frame

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,23 @@
-import React from "react"
+import React from 'react'
 
 export default () => {
-    return (
-        <div>
-            Welcome Home!
-        </div>
-    )
+  return (
+    <div>
+      <iframe
+        id='forum_embed'
+        title="myFrame"
+        src='javascript:void(0)'
+        scrolling='no'
+        frameborder='0'
+        width='900'
+        height='700'
+      />
+      <script type='text/javascript'>
+        document.getElementById('forum_embed').src =
+        'https://groups.google.com/forum/embed/?place=forum/elta-cafe' +
+        '&showsearch=true&showpopout=true&showtabs=false' + '&parenturl=' +
+        encodeURIComponent(window.location.href);
+      </script>{' '}
+    </div>
+  )
 }
