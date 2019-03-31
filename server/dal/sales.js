29358074e82681708d1c813dd63f9ed5ed1daffc
Food Factory Business Automation Home <ffba@labshome.co>
2019-03-31 11:17:16 +0200

    sales totals extended to 3 months

--- server/dal/sales.js
+++ server/dal/sales.js
@@ -8,7 +8,7 @@ const sales = require("../sql/sales")
 module.exports = {
 
   daily_totals_for_period: async (db, params) => {
-    const date_0 = params.date0 || moment().utc().startOf("month").subtract(2, "month").toISOString()
+    const date_0 = params.date0 || moment().utc().startOf("month").subtract(3, "month").toISOString()
     const date_1 = params.date1 || moment().utc().endOf("day").toISOString()
     const daily_totals = await db.any(sales.daily_totals_for_period, {
       ...params, date_0, date_1,
